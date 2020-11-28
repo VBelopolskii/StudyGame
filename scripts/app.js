17a56d271adf7b7491d2ce7dfe160da79d4d088c
valerii.belopolskii <valerii.belopolskii@crplab.ru>
2020-11-28 07:25:46 +0300

    JS approach

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,42 @@
 const score = document.getElementById("score");
 const record = document.getElementById("record");
+let timeoutId = 0;
+let square1 = document.getElementsByClassName('ui-figure1')[0];
 
-function clickMessage() {
-    console.log("hello")
-}
 
 function remove(el) {
-    let figure = el;
-    console.log(figure.nextElementSibling)
-    if (figure.nextElementSibling != null) {
-        figure.nextElementSibling.style.animationDelay = "0s";
-        figure.nextElementSibling.style.left = "-50px";
-    }
-    figure.remove();
+    // let figure = el;
+    // console.log(figure.nextElementSibling)
+    // if (figure.nextElementSibling != null) {
+    //     figure.nextElementSibling.style.animationDelay = "0s";
+    //     figure.nextElementSibling.style.left = "-50px";
+    // }
+    // figure.remove();
+    console.log('test')
 }
+
+function start() {
+    timeoutId = setInterval(move, 10);
+
+}
+
+function move() {
+    let x = square1.offsetLeft;
+    let y = square1.offsetTop;
+    square1.style.left = `${x + 1}px`;
+    // console.log(square1)
+}
+
+function stop() {
+    clearTimeout(timeoutId)
+}
+
+function click(event) {
+    console.log(this, event);
+    this.remove();
+}
+square1.addEventListener('click', click)
+
+
+start();
+console.log(stop)
