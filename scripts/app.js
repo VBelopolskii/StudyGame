f0e1f521d2bddb08db2cb812045cd5ab1cb54707
valerii.belopolskii <valerii.belopolskii@crplab.ru>
2020-12-04 22:11:03 +0300

    added methods for renaming the button and stopping/starting the game

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,8 @@ const gameField = document.getElementById('playField');
 let timeoutId;
 let figure = createFigure();
 startBtn = document.getElementsByClassName('js-btn-start')[0];
-pauseBtn = document.getElementsByClassName('js-btn-pause')[0];
+
+// pauseBtn = document.getElementsByClassName('js-btn-pause')[0];
 
 function move() {
     let elem = document.getElementsByClassName('js-figure')[0];
@@ -13,15 +14,12 @@ function move() {
     elem.style.left = `${x + 1}px`;
 }
 
-function changeFigure() {
-}
-
-function startRenameToContinue() {
-    startBtn.textContent = 'Continue'
-}
-
 function stop() {
-    clearTimeout(timeoutId)
+    clearInterval(timeoutId)
+}
+
+function start() {
+    timeoutId = setInterval(move, 10);
 }
 
 function createFigure() {
@@ -31,18 +29,32 @@ function createFigure() {
     return newFigure;
 }
 
-function click(event) {
+function clickFigure() {
     let newFigure = createFigure();
     this.remove();
-    newFigure.addEventListener('click', click);
+    newFigure.addEventListener('click', clickFigure);
 }
 
-function start() {
-    timeoutId = setInterval(move, 10);
+function changeFigureForm() {
 }
 
-figure.addEventListener('click', click)
-startBtn.addEventListener('start', click)
-pauseBtn.addEventListener('stop', click)
-pauseBtn.addEventListener('startRenameToContinue', click)
+function startPauseRename() {
+    console.log(startBtn.textContent);
+    startBtn.textContent === "Start" ? startBtn.textContent = "Pause" : startBtn.textContent = "Start";
+}
+
+function clickStartBtn() {
+    if (startBtn.textContent === "Start") {
+        start();
+        console.log(timeoutId);
+    } else {
+        stop();
+        console.log(timeoutId);
+    }
+    startPauseRename();
+}
+
+
+figure.addEventListener('click', clickFigure);
+startBtn.addEventListener('click', clickStartBtn);
 
